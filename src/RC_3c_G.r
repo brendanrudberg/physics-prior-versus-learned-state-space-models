921f187839b98e5b50d75aeae2d4741339db71f2
brendanrudberg <rudbergbrendan@gmail.com>
2026-08-16 16:12:38 +0000

    RC_3c_G.r

--- src/RC_3c_G.r
+++ src/RC_3c_G.r
@@ -1,14 +1,17 @@
 library(ctsmTMB)
 
-build_2c_G <- function(data) {
+build_3c_G <- function(data) {
 
   model <- ctsmTMB$new()
 
   #Inner wall temperature ODE
-  model$addSystem(dTwi ~ ((Tai - Twi)/(R1*(f*Ctot)) + (Two - Twi)/(R2*(f*Ctot))) * dt + exp(p1)*dw1)
+  model$addSystem(dTwi ~ ((Tai - Twi)/(R1*(f1*Ctot)) + (Twm - Twi)/(R2*(f1*Ctot))) * dt + exp(p1)*dw1)
+
+  #Middle wall temperature ODE
+  model$addSystem(dTwm ~ ((Twi - Twm)/(R2*(f2*(1-f1)*Ctot)) + (Two - Twm)/(R3*(f2*(1-f1)*Ctot))) * dt + exp(p3)*dw3)
 
   #Outer wall temperature ODE
-  model$addSystem(dTwo ~ ((Twi - Two)/(R2*(Ctot-f*Ctot)) + (Tao - Two)/(R3*(Ctot-f*Ctot)) + (Aw*G)/(Ctot-f*Ctot)) * dt + exp(p2)*dw2)
+  model$addSystem(dTwo ~ ((Twm - Two)/(R3*((1-f2)*(1-f1)*Ctot)) + (Tao - Two)/(R4*((1-f2)*(1-f1)*Ctot)) + (Aw*G)/((1-f2)*(1-f1)*Ctot)) * dt + exp(p2)*dw2)
 
   # Adding observations based on the measured wall temperatures
   model$addObs(yTwi ~ Twi)
@@ -23,19 +26,22 @@ build_2c_G <- function(data) {
 
   model$setParameter(
     Ctot = c(initial = 0.30 * 2400 * 880 / 3.6e6), #total wall heat capacity (estimated constant)
-    f   = c(initial = 0.5,  lower = 1e-3, upper = 0.999), #ratio between different wall section heat capacities
+    f1 = c(initial = 0.33, lower = 1e-3, upper = 0.999), #ratio between different wall section heat capacities
+    f2 = c(initial = 0.5,  lower = 1e-3, upper = 0.999), #ratio between different wall section heat capacities
     R1  = c(initial = 1,  lower = 1e-3, upper = 1e3), # thermal resistance of inner wall
     R2  = c(initial = 1,  lower = 1e-3, upper = 1e3), # thermal resistance of middle layer
     R3  = c(initial = 1,  lower = 1e-3, upper = 1e3), # thermal resistance of outer wall
+    R4  = c(initial = 1,  lower = 1e-3, upper = 1e3), # thermal resistance of outer wall
     p1  = c(initial = -1, lower = -30, upper = 10), # process noise for inner wall
     p2  = c(initial = -1, lower = -30, upper = 10), # process noise for outer wall
+    p3  = c(initial = -1, lower = -30, upper = 10), # process noise for middle wall
     e1 = c(initial = -8), # measurement noise for inner wall (estimated constant)
     e2 = c(initial = -8), # measurement noise for outer wall (estimated constant)
     Aw = c(initial = 0.5, lower = 0, upper = 10) # Solar aperature constant
   )
 
-  model$setInitialState(list(c(data$yTwi[1], data$yTwo[1]), diag(2))) # Setting the initial state 
-  # of the system based on the first measured values of Twi and Two, with a diagonal covariance matrix.
+  mid0 <- mean(c(data$yTwi[1], data$yTwo[1]))
+  model$setInitialState(list(c(data$yTwi[1], mid0, data$yTwo[1]), diag(3)))
 
   model
 }
--- src/main.r
+++ src/main.r
@@ -1,10 +1,19 @@
-source("diag.r") 
-source("RC_2c.r")
-source("RC_family.r")
+source("src/diag.r") 
+source("src/RC_2c.r")
+source("src/RC_family.r")
+source("src/RC_2c_G.r")
 
-data <- readRDS("processed_data/temp_log_8_5_trimmed_2.rds")
-model <- build_2c(data)
-fit <- fit_model(model, data, "processed_data/fit_wall_2c_Ctot_e-8.rds")
+
+#data <- readRDS("processed_data/temp_log_8_5_trimmed_2.rds")
+#model <- build_2c(data)
+#fit <- fit_model(model, data, "processed_data/fit_wall_2c_Ctot_e-8_solar.rds")
+
+
+data <- readRDS("processed_data/temp_log_8_5_solar.rds")
+model <- build_2c_G(data)
+fit <- fit_model(model, data, "processed_data/fit_wall_2c_Ctot_e-8_solar.rds")
+
+
 fit_summary <- summary(fit)
 fit_cor <- cov2cor(fit$cov.fixed)
 res <- residuals(fit)
